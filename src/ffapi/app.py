24bf8c6209a2f507f8b161e357faaaf9ce573df5
Nicou11 <lyoungin1204@gmail.com>
2024-08-12 16:29:35 +0900

    README

--- src/ffapi/app.py
+++ src/ffapi/app.py
@@ -1,6 +1,7 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import pandas as pd
 
 app = FastAPI()
 
@@ -9,7 +10,25 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/sample")
+def sample_data():
+    df = pd.read_parquet('/home/young12/code/ffapi/data')
+    sample_df = df.sample(n=5)
+    r = sample_df.to_dict(orient='records')
+    return r
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/movie/{movie_cd}")
+def movie_meta(movie_cd: str):
+    df = pd.read_parquet('/home/young12/code/ffapi/data')
+
+    # df에서 movieCd == movie_cd row를 조회 df[['a'] === b] ...
+    meta_df = df[df['movieCd'] == movie_cd]
+
+    if meta_df.empty:
+        raise HTTPException(status_code=404, detail="영화를 찾을 수 없습니다.")
+
+    # 조회된 데이터를 .to_dict()로 만들어 아래에서 return
+    r = meta_df.iloc[0].to_dict()
+
+    return r
+
